72640420e3b27a93768660b2679e448a9e20725e
SadevMK <111415549+SadevMK@users.noreply.github.com>
2025-04-15 02:04:10 +0530

    Reupdate

--- builder/init.js
+++ builder/init.js
@@ -8,58 +8,92 @@ const {
   generateMarkupRemote,
 } = require("./generateMarkup");
 
-require("dotenv").config();
+// Hardcoded values for testing
+const NAME = "Piyumi"; // Replace with the name of the recipient
+const PIC = "sample-pic.jpeg"; // Replace with the image file name
+const NICKNAME = "Buddy"; // Replace with a nickname
+const HBD_MSG = "May your soul rest in peace"; // Replace with your greeting message
+const SCROLL_MSG = "sample-scroll.txt"; // Replace with the text file containing the scroll message
 
-if (!process.env.NAME) throw new Error("Please specify NAME in environment.");
-if (!process.env.PIC) throw new Error("Please specify PIC in environment.");
+// Debugging: Log values at the start
+console.log("Debug Info:");
+console.log("NAME:", NAME);
+console.log("PIC:", PIC);
+console.log("NICKNAME:", NICKNAME);
+console.log("HBD_MSG:", HBD_MSG);
+console.log("SCROLL_MSG:", SCROLL_MSG);
 
-const picPath = process.env.PIC;
-const msgPath = process.env.SCROLL_MSG;
-
-//Local initialization
+// Local initialization
 const setLocalData = async () => {
   try {
-    const pic = path.join(__dirname, "../local/", picPath);
+    console.log("Starting local initialization...");
+    const pic = path.join(__dirname, "../local/", PIC);
+    console.log("PIC Path:", pic);
+
     let markup = "";
-    if (msgPath) {
-      const text = fs.readFileSync(path.join(__dirname, "../local/", msgPath), {
+    if (SCROLL_MSG) {
+      const text = fs.readFileSync(path.join(__dirname, "../local/", SCROLL_MSG), {
         encoding: "utf-8",
       });
+      console.log("Scroll Message Content:", text);
       markup = generateMarkupLocal(text);
     }
+    console.log("Generated Markup:", markup);
+
     await setPic(pic);
+    console.log("Picture has been processed.");
     genIndex(markup);
+    console.log("Index file has been generated.");
   } catch (e) {
-    throw new Error(e.message);
+    console.error("Error during local initialization:", e.message);
   }
 };
 
-//Remote initialization
+// Remote initialization
 const setRemoteData = async () => {
   try {
-    let res = await axios.get(picPath, {
+    console.log("Starting remote initialization...");
+    console.log("Fetching picture from remote path:", PIC);
+
+    let res = await axios.get(PIC, {
       responseType: "arraybuffer",
     });
     const pic = res.data;
+    console.log("Fetched remote picture data.");
+
     let markup = "";
-    if (msgPath) {
-      const article = msgPath.split("/").pop();
+    if (SCROLL_MSG) {
+      const article = SCROLL_MSG.split("/").pop();
+      console.log("Fetching scroll message content from remote path...");
       res = await axios.get(
         `https://api.telegra.ph/getPage/${article}?return_content=true`
       );
       const { content } = res.data.result;
+      console.log("Fetched scroll message content:", content);
+
       markup = content.reduce(
         (string, node) => string + generateMarkupRemote(node),
         ""
       );
     }
+    console.log("Generated Markup:", markup);
+
     await setPic(pic);
+    console.log("Picture has been processed.");
     genIndex(markup);
+    console.log("Index file has been generated.");
   } catch (e) {
-    throw new Error(e.message);
+    console.error("Error during remote initialization:", e.message);
   }
 };
 
-if (process.argv[2] === "--local") setLocalData();
-else if (process.argv[2] === "--remote") setRemoteData();
-else console.log("Fetch mode not specified.");
+// Determine mode of initialization
+if (process.argv[2] === "--local") {
+  console.log("Running in Local Mode...");
+  setLocalData();
+} else if (process.argv[2] === "--remote") {
+  console.log("Running in Remote Mode...");
+  setRemoteData();
+} else {
+  console.log("Fetch mode not specified.");
+}
